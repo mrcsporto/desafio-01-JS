e6873dc6f15caa49a1fd3ee1a9fcc793b7ea1650
Marcos Porto <89549750+mrcsporto@users.noreply.github.com>
2022-05-15 10:34:38 -0300

    Update challenge_3.js

--- challenge_3.js
+++ challenge_3.js
@@ -1,8 +1,8 @@
 let vendas = [100, 40.3, 51.4 , 200, 49.9, 30]
-let comissao = 0;
+let comissao = 0
 
 for (let i = 0; i < vendas.length; i++) {
-    comissao += vendas[i]*0.05;
+    comissao += vendas[i]*0.05
     if(comissao > 150){
         console.log('O vendedor atingiu a meta!')
     } else {
