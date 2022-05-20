145a2d34be2b1a4f4c0ce5610fc9682ec0af6021
Marcos Porto <89549750+mrcsporto@users.noreply.github.com>
2022-05-20 14:30:50 -0300

    Update challenge_1.js

--- challenge_1.js
+++ challenge_1.js
@@ -2,7 +2,7 @@ let inArray = [false,'text_01',99,true,'text_02',285]
 console.log(inArray)
 
 for(let i = 0; i < inArray.length ; i++) {
-    if(typeof inArray[i] === 'string' || typeof inArray[i] === 'number'){
+    if(typeof inArray[i] == 'string' || typeof inArray[i] == 'number'){
         console.log(inArray[i])
     }
 }
