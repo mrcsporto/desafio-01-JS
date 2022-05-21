2ef779b5441c590ad045028d59e763e59bcff795
Marcos Porto <89549750+mrcsporto@users.noreply.github.com>
2022-05-21 13:53:58 -0300

    Update challenge_2.js

--- challenge_2.js
+++ challenge_2.js
@@ -1,12 +1,11 @@
 let firstArray = [[1,9], [2,6], [3,4], [6,7]]
+let secondArray = [[1,9], [2,6], [3,4], [6,7,3,4,5]]
 
 for(let i = 0; i < firstArray.length ; i++){ 
     for(let j = 0; j < firstArray[i].length ; j++){
         console.log(firstArray[i][j])
     }  
 }
-
-let secondArray = [[1,9], [2,6], [3,4], [6,7,3,4,5]]
 
 for(let k = 0; k < secondArray.length ; k++){ 
     for(let l = 0; l < secondArray[k].length ; l++){
